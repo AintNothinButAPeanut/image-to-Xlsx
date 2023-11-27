b91bcdcd78d83228e32a0b6fc94182f081072876
narcissusTheFlower <narcissus_mail@mail.ru>
2023-11-28 01:30:04 +0300

    Python script fix

--- python_mapper.py
+++ python_mapper.py
@@ -5,31 +5,31 @@ from datetime import datetime
 from openpyxl import Workbook
 from openpyxl.drawing.image import Image as OpenpyxlImage
 from openpyxl.styles import Alignment, Font
+from pathlib import Path
 
 # Get directory with files
-sourceDirectory = sys.argv[1]
-targetDirectory = sys.argv[2]
-identifierForController = sys.argv[3]
+sourceDirectory = sys.argv[1]  # directory with pictures
+targetDirectory = sys.argv[2]  # directory where to save excel file
+identifierForController = sys.argv[3]  # identirifer for ITE (optional but still need a string, like "")
 directory = os.fsdecode(sourceDirectory)
 filesCount = len(os.listdir(directory))
 pictureFileCount = 0
 
 # Get text and picture info from each file
-eachFileText = []
-eachFileImage = []
+eachTxtFile = []  # Intended to hold the txt file
+eachFilesImage = []  #Intended to hold the image file
 maxPictureWidth = 0
 maxPictureHeight = 0
 
 for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith(".txt"):
-        thisFilesText = open(directory + "/" + filename, 'r')
-        eachFileText.append(thisFilesText.read())
-    elif filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".tiff"):
+    txtFileNAme = os.fsdecode(file)
+    if txtFileNAme.endswith(".txt"):
+        eachTxtFile.append(directory + "/" + txtFileNAme)
+    elif txtFileNAme.endswith(".jpg") or txtFileNAme.endswith(".png") or txtFileNAme.endswith(".tiff"):
         pictureFileCount += 1
         # Pillow Image class
-        with Image.open(directory + "/" + filename) as image:
-            eachFileImage.append(directory + "/" + filename)
+        with Image.open(directory + "/" + txtFileNAme) as image:
+            eachFilesImage.append(directory + "/" + txtFileNAme)
             width, height = image.size
             if width > maxPictureWidth:
                 maxPictureWidth = width
@@ -61,19 +61,35 @@ myAlignment = Alignment(horizontal='general',
                         shrink_to_fit=False,
                         indent=0)
 
+# Define and prepare the dictionary by matching filenames
+picTextDictionary = {}
+
+for txtFileName in eachTxtFile:
+    pureImageFileName = txtFileName.split("/")
+    pureFileName = Path(pureImageFileName[-1]).stem
+    for imageFileName in eachFilesImage:
+        pureImageFileName = imageFileName.split("/")
+        pureImageFileName = Path(pureImageFileName[-1]).stem
+        if (pureFileName == pureImageFileName):
+            picTextDictionary[txtFileName] = imageFileName
+
+eachText = list(picTextDictionary.keys())
+eachImage = list(picTextDictionary.values())
+
 # First column
-for i in range(len(eachFileText)):
+for i in range(len(eachText)):
     # Set cell parameters/style
     ws.row_dimensions[i + 1].height = maxPictureHeight
     ws['A' + str(i + 1)].alignment = myAlignment
     ws['A' + str(i + 1)].font = myFont
 
     # Write text to cell
-    ws['A' + str(i + 1)] = eachFileText[i]
+    text = open(eachText[i], 'r')
+    ws['A' + str(i + 1)] = text.read()
 
 # Second column
-for i in range(len(eachFileImage)):
-    img = OpenpyxlImage(eachFileImage[i])
+for i in range(len(eachImage)):
+    img = OpenpyxlImage(eachImage[i])
     ws.add_image(img, 'B' + str(i + 1))
 
 # Works
